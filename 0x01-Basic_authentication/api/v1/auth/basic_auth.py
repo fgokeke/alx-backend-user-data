eb121067abbd8bee27e6c62186395c2389d19bb4
root <okekefeargod>
2024-05-21 23:57:29 +0000

    6. Basic auth 2

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,10 +6,10 @@ This module defines a BasicAuth class for basic authentication.
 """
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """
     BasicAuth class for basic authentication.
-    
     Inherits from Auth and currently does not add any new functionality.
     """
     pass
