b187d209266e3067d3431ce3755ae229f8309c74
fgokeke <okekefeargod@gmail.com>
2024-05-21 22:53:10 +0000

    6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic authentication module.
+
+This module defines a BasicAuth class for basic authentication.
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class for basic authentication.
+    
+    Inherits from Auth and currently does not add any new functionality.
+    """
+    pass
