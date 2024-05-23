6cf0891a123f77f894c2c9e12c676aa1253bc996
root <okekefeargod>
2024-05-23 20:25:31 +0000

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Session-based authentication."""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class for session-based authentication."""
+    pass
