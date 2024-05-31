571d021dce40e28de041f8ee00461db1a587c830
root <okekefeargod>
2024-05-31 00:41:20 +0000

    9. Generate UUIDs 3

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,7 +22,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """Generate a new UUID.
 
     Returns:
